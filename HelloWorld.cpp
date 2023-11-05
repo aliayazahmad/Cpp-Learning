692a7dfea7b58da8dfaacbea23ecd93413e491bd
ali ayaz ahmad <aliayazahmad731@gmail.com>
2023-11-05 20:19:21 +0530

    New code has been updated

---
+++ HelloWorld.cpp
@@ -0,0 +1,39 @@
+// #include <iostream>
+// #include <vector>
+// #include <string>
+
+// using namespace std;
+
+// int main()
+// {
+//     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+//     for (const string& word : msg)
+//     {
+//         cout << word << " ";
+//     }
+//     cout << endl;
+// }
+// // Factorial
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long factorial = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;    
+    }
+
+    return 0;
+}
